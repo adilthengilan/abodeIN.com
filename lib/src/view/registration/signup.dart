6582abe1c655a41815ebf9bc9c55f05dd47694b7
NaheelJ <naheeljnaheel5454@gmail.com>
2024-05-06 12:13:04 +0530

    Naheel

--- lib/src/view/registration/signup.dart
+++ lib/src/view/registration/signup.dart
@@ -1,4 +1,4 @@
-import 'package:abodein/src/Utils/app_colors.dart';
+import 'package:abodein/utils/app_colors.dart';
 import 'package:abodein/utils/style.dart';
 import 'package:abodein/src/view/common_Widgets/text_button.dart';
 import 'package:abodein/src/view/common_Widgets/text_field.dart';
