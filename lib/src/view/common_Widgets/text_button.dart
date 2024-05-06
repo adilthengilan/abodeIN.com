1bde9af1f38d46eef796498850c0be4c644c2674
NaheelJ <naheeljnaheel5454@gmail.com>
2024-05-06 10:29:41 +0530

    Naheel

--- lib/src/view/common_Widgets/text_button.dart
+++ lib/src/view/common_Widgets/text_button.dart
@@ -1,4 +1,4 @@
-import 'package:abodein/src/Utils/app_colors.dart';
+import 'package:abodein/utils/app_colors.dart';
 import 'package:abodein/utils/style.dart';
 import 'package:flutter/material.dart';
 
