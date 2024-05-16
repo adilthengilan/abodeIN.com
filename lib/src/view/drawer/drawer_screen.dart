1333757263ac327847ada78be48f5bfead926540
AyeshaNuhha <ayeshakorambayil@gmail.com>
2024-05-16 14:27:16 +0530

    Drawer  14:27

--- lib/src/view/drawer/drawer_screen.dart
+++ lib/src/view/drawer/drawer_screen.dart
@@ -13,36 +13,36 @@ class DrawerScreen extends StatefulWidget {
 }
 
 class _DrawerScreenState extends State<DrawerScreen> {
-  //============================== Items
-  List<String> items = [
-    " H O M E",
-    " P R O F I L E",
-    " H I S T O R Y",
-    " R E W A R D S",
-    " F A V O U R I T E S",
-    " S E T T I N G S",
-    " L O G O U T",
-  ];
-//============================ Icons
-  List<IconData> icones = [
-    Icons.home_outlined,
-    Icons.person_2_outlined,
-    Icons.history,
-    Icons.card_giftcard_outlined,
-    Icons.favorite_outline,
-    Icons.settings_outlined,
-    Icons.logout_outlined,
-  ];
-  //========================= Routes
-  List<Widget> route = [
-    DashBoard(),
-    HotelDetailePage(),
-    HotelDetailePage(),
-    HotelDetailePage(),
-    HotelDetailePage(),
-    HotelDetailePage(),
-    HotelDetailePage(),
-  ];
+//   //============================== Items
+//   List<String> items = [
+//     " H O M E",
+//     " P R O F I L E",
+//     " H I S T O R Y",
+//     " R E W A R D S",
+//     " F A V O U R I T E S",
+//     " S E T T I N G S",
+//     " L O G O U T",
+//   ];
+// //============================ Icons
+//   List<IconData> icones = [
+//     Icons.home_outlined,
+//     Icons.person_2_outlined,
+//     Icons.history,
+//     Icons.card_giftcard_outlined,
+//     Icons.favorite_outline,
+//     Icons.settings_outlined,
+//     Icons.logout_outlined,
+//   ];
+//   //========================= Routes
+//   List<Widget> route = [
+//     DashBoard(),
+//     HotelDetailePage(),
+//     HotelDetailePage(),
+//     HotelDetailePage(),
+//     HotelDetailePage(),
+//     HotelDetailePage(),
+//     HotelDetailePage(),
+//   ];
 
   @override
   Widget build(BuildContext context) {
@@ -51,34 +51,75 @@ class _DrawerScreenState extends State<DrawerScreen> {
     return Drawer(
       backgroundColor: transparantdrawerColor,
       width: width * 0.84,
-      child: Column(children: [
-        //=========================== Profile Section =====================================
-        sizedBox(height * 0.10, width),
-        _buildheader(height, width),
-        //================================ Build Items ===================================
-        Expanded(
-          child: ListView.builder(
-            itemCount: items.length,
-            itemBuilder: (context, i) {
-              return ListTile(
-                  title: Text(
-                    items[i],
-                    style: whiteTextStyle,
-                  ),
-                  leading: Icon(
-                    icones[i],
-                    color: backgroundColor,
-                  ),
-                  trailing: Icon(
-                    Icons.chevron_right,
-                    color: backgroundColor,
-                  ),
-                  onTap: () => Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => route[i])));
-            },
-          ),
-        ),
-      ]),
+      child: ListView(
+        children: [
+          //=============== Profile =======================
+          sizedBox(height * 0.050, width),
+          _buildheader(height, width),
+          //================= Home =================
+          sizedBox(height * 0.050, width),
+
+          _buildItem(
+              icon: Icons.home_outlined, title: " H O M E", onTap: () {}),
+
+          //====================== Profile ===========================
+          _buildItem(
+              icon: Icons.person_2_outlined,
+              title: " P R O F I L E",
+              onTap: () {}),
+          //==================Notification ===================
+          _buildItem(
+              icon: Icons.history, title: " H I S T O R Y", onTap: () {}),
+          //=============================== Discover ==========================
+          _buildItem(
+              icon: Icons.card_giftcard_outlined,
+              title: " R E W A R D S",
+              onTap: () {}),
+          //=========================Collections ==============================
+          _buildItem(
+              icon: Icons.favorite_outline,
+              title: " F A V O U R I T E S",
+              onTap: () {}),
+          //=============================Settings=============================
+          _buildItem(
+              icon: Icons.settings_outlined,
+              title: " S E T T I N G S",
+              onTap: () {}),
+
+          //======================== Logout =============================
+          _buildItem(
+              icon: Icons.logout_outlined, title: " L O G O U T", onTap: () {})
+        ],
+      ),
+
+      // child: Column(children: [
+      //   //=========================== Profile Section =====================================
+      //   sizedBox(height * 0.10, width),
+      //   _buildheader(height, width),
+      //   //================================ Build Items ===================================
+      //   Expanded(
+      //     child: ListView.builder(
+      //       itemCount: items.length,
+      //       itemBuilder: (context, i) {
+      //         return ListTile(
+      //             title: Text(
+      //               items[i],
+      //               style: whiteTextStyle,
+      //             ),
+      //             leading: Icon(
+      //               icones[i],
+      //               color: backgroundColor,
+      //             ),
+      //             trailing: Icon(
+      //               Icons.chevron_right,
+      //               color: backgroundColor,
+      //             ),
+      //             onTap: () => Navigator.push(context,
+      //                 MaterialPageRoute(builder: (context) => route[i])));
+      //       },
+      //     ),
+      //   ),
+      // ]),
     );
   }
 
@@ -97,6 +138,32 @@ class _DrawerScreenState extends State<DrawerScreen> {
           style: whiteMediumTextStyle,
         )
       ],
+    );
+  }
+
+//=================================== Build Items ===================================
+  _buildItem({
+    required IconData icon,
+    required String title,
+    required GestureTapCallback onTap,
+  }) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        color: backgroundColor,
+      ),
+      title: Text(
+        title,
+        style: whiteTextStyle,
+      ),
+      trailing: InkWell(
+        onTap: onTap,
+        child: Icon(
+          Icons.arrow_forward_ios,
+          color: backgroundColor,
+        ),
+      ),
+      minLeadingWidth: 5,
     );
   }
 }
