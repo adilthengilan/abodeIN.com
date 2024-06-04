5c0799e64c8d89b8aae50df3b53bb1937494c4ba
AyeshaNuhha <ayeshakorambayil@gmail.com>
2024-06-04 21:48:15 +0530

    roomms

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           debugShowCheckedModeBanner: false,
-          home: HotelRoomsScreen(height: , width: width)),
+          home: HotelRoomsScreen()),
     );
   }
 }
