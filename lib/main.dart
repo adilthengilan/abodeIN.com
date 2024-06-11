a9980759e461af79befa61898fbd8a8800eb78ee
AyeshaNuhha <ayeshakorambayil@gmail.com>
2024-06-11 22:30:54 +0530

    mybookingscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:abodein/src/view/About%20us/about_us.dart';
 import 'package:abodein/src/view/About%20us/terms_and_conditions.dart';
 import 'package:abodein/src/view/dashBoard/dashboard_screen.dart';
+import 'package:abodein/src/view/hotel_rooms/rooms_details_screen.dart';
 import 'package:abodein/src/view/mybooking/my_booking_screen.dart';
 import 'package:abodein/src/view/settings/settings.dart';
 import 'package:abodein/utils/providers.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
           ),
           debugShowCheckedModeBanner: false,
-          home: TermsAndConditions()),
+          home: MyBookingScreen()),
     );
   }
 }
