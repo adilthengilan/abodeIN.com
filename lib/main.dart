3058188ba1647ea24b0444223033833de8c92817
AyeshaNuhha <ayeshakorambayil@gmail.com>
2024-06-13 13:48:27 +0530

    rewards updated 13/06

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
           debugShowCheckedModeBanner: false,
-          home: DashBoard()),
+          home:RewardScreen()),
     );
   }
 }
